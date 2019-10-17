333dba09a185571237042e6c805b7cc148e09547
chrisholt0222 <50928045+chrisholt0222@users.noreply.github.com>
2019-10-17 15:18:29 -0400

    update

---
+++ Leaflet-Step-2/static/js/logic.js
@@ -0,0 +1,148 @@
+// Set Options range and colors
+var labels = ["Mag: 0-1", "Mag: 1-2", "Mag: 2-3", "Mag: 3-4", "Mag: 4-5", "Mag: 5+"];
+var optColors = ["green", "lightgreen", "gold", "orange", "orangered", "crimson"];
+
+// Load in geojson data
+//var geoData = "static/data/1.geojson";
+var boundary = "static/data/PB2002_boundaries.json";
+var plates = "static/data/PB2002_plates.json";
+
+var geoData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
+
+//Set Color based on Magnitude of EQ
+function setColor(magnitude) {
+    var ind = 5;
+    for (i = 0; i < optColors.length; i++){ if (magnitude<i+1) {ind = i; break;} }   
+    return optColors[Math.min(5,ind)];
+    };
+
+// Set the legend
+function createLegend(getMap){
+    //Create HTML place holder for legend
+    var info = L.control({ position: "bottomright" })
+    info.onAdd = function(){
+        var div = L.DomUtil.create("div","legend");
+        return div;
+        };
+    info.addTo(getMap);
+
+    // Create Legend table
+    var setHtml = "<div>Legend</div>";    
+    for (i = 0; i < labels.length; i++){
+        setHtml += "<div style = \"background-color: " + optColors[i] + "\">" + labels[i] + "</div> ";    
+        };
+    document.querySelector(".legend").innerHTML = [setHtml].join();
+    };
+
+//Set the size, color, location of EQ circle 
+function createCircles(earthquakeData) {
+    var EQ = L.geoJson(earthquakeData,{
+        pointToLayer: function (feature, latlng) {
+            var mag = feature.properties.mag
+            var options = {
+                radius: mag * mag / 1.25,
+                fillColor: setColor(mag),
+                color: "black",
+                weight: 1,
+                opacity: 1,
+                fillOpacity: 0.6
+            };
+            
+          return L.circleMarker(latlng, options)
+            .bindPopup("<p>" + "Location: " + feature.properties.place +
+            "<br> Date/Time: " + new Date (feature.properties.time) +
+            "<br> Magnitude: " + feature.properties.mag + "</p>");
+        }
+      });
+    return EQ
+};
+
+function createBoundary() {
+  d3.json(boundary, function(response) {
+    var myLayer = L.geoJSON();
+    return myLayer.addData(response.features);
+  });
+};
+
+// Grab data with d3
+d3.json(geoData, function(response) {
+        
+  var earthquakes = createCircles(response.features);
+
+  // Adding tile layers
+  var streetLevel = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "mapbox.streets",
+        accessToken: API_KEY
+      });
+    
+  var pirates = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.pirates",
+    accessToken: API_KEY
+  });
+
+  var outdoors = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.outdoors",
+    accessToken: API_KEY
+  });
+
+  var satellite = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.satellite",
+    accessToken: API_KEY
+  });
+
+  // Create a baseMaps object to hold the lightmap layer
+  var baseMaps = {
+    "Street Level" : streetLevel,
+    "Pirates": pirates,
+    "Outdoors" : outdoors,
+    "Satellite" : satellite
+   };
+
+  //Get Boundary and Plate jsons
+  var myBoundary = L.geoJSON();
+  d3.json(boundary, function(data) {
+    var myStyle = {
+      "color": "orange",
+      "weight": 5,
+      "opacity": 0.5
+    };
+
+    myBoundary.addData(data.features, {style: myStyle});
+    });
+  
+/*  var myPlates = L.geoJSON();
+  d3.json(plates, function(data) {
+    myPlates.addData(data.features);
+    });
+*/
+
+  // Create an overlayMaps object to hold the bikeStations layer
+  var overlayMaps = {
+    "Earthquakes": earthquakes,
+    "Boundary" : myBoundary
+  };
+
+  // Create the map object with options
+  var myMap = L.map("map", {
+    center: [41.2565, -95.9345],
+    zoom: 4,
+    layers: [streetLevel, earthquakes, myBoundary]
+  });
+
+  // Create a layer control, pass in the baseMaps and overlayMaps. Add the layer control to the map
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(myMap);
+
+// Display legend
+  createLegend(myMap); 
+
+});
--- README.md
+++ README.md
@@ -6,4 +6,4 @@ The United States Geological Survey (USGS) collects and provides scientific data
 
 Live site: https://chrisholt0222.github.io/leaflet-challenge/
 
-The plottedd cirles reflect the magnitude of the earthquake in both size and color. Earthquakes with higher magnitudes appear larger and darker in color. Additional information is included in popups for each earthquake.
+There are two verions: Leaflet-Step-1 and Leaflet-Step-2. Both plot cirles on the world map reflect the magnitude of the earthquake in both size and color. Earthquakes with higher magnitudes appear larger and darker in color. Additional information is included Leaflet-Step-2 (as it expands on Leaflet-Step-1) such as popups on each earthquake and the ability to fault line boundaries on the map.
